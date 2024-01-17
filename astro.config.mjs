d22adc164652a37af4bf6e90e8d557d5a2bff905
Alberto813 <albertomontiel813@gmail.com>
2024-01-17 08:41:30 -0800

    haciendo deploy para github pages

--- astro.config.mjs
+++ astro.config.mjs
@@ -4,4 +4,6 @@ import tailwind from "@astrojs/tailwind";
 // https://astro.build/config
 export default defineConfig({
   integrations: [tailwind()],
+  site: 'https://alberto813.github.io',
+  base: '',
 });
