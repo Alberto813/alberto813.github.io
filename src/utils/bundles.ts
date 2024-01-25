6d31f28c934e1ba07f26d53d1ca2fb77e3dec709
Alberto813 <albertomontiel813@gmail.com>
2024-01-24 23:41:25 -0800

    Update some info about bundles, like Id for dinamic builds

--- src/utils/bundles.ts
+++ src/utils/bundles.ts
@@ -1,6 +1,7 @@
 const bundles = [
     {
-        title: "paquete 1",
+        id:"paquete1",
+        title: "Paquete 1",
         description: "Titulo del paquete",
         itemsIncluded: ["2 Pizzas XL","10L de bebida","20 Minis de 10 fichas","35 Fichas para cumpleanero","Regalo sorpresa","Anfitriona","4Hrs de evento","Desechables para comida","Mobiliario","Servicio de limpieza","Mantel para mesa principal","20 Postres de nieve"],
         icon: `<svg xmlns= width="24" height="24" fill="currentColor"><path d="M9.76 2.021a.995.995 0 0 0-.989.703L3.579 19.166a1 1 0 0 0 1.255 1.255l16.442-5.192a.991.991 0 0 0 .702-.988C21.6 7.666 16.334 2.4 9.76 2.021zM10 16a2 2 0 1 1 .001-4.001A2 2 0 0 1 10 16zm6-2a2 2 0 1 1 .001-4.001A2 2 0 0 1 16 14z"></path></svg>`,
@@ -8,7 +9,8 @@ const bundles = [
         price:"$4,000 MXN",
     },
     {
-        title: "paquete 2",
+        id:"paquete2",
+        title: "Paquete 2",
         description: "Hotdogs ",
         itemsIncluded: ["42 Hotdogs","10L de bebida","20 Minis de 20 fichas","35 Fichas para cumpleanero","Regalo sorpresa","Anfitriona","4Hrs de evento","Desechables para comida","Mobiliario","Servicio de limpieza","Mantel para mesa principal","20 Postres de nieve"],
         icon: `<svg xmlns= width="24" height="24" fill="currentColor"><path d="M9.76 2.021a.995.995 0 0 0-.989.703L3.579 19.166a1 1 0 0 0 1.255 1.255l16.442-5.192a.991.991 0 0 0 .702-.988C21.6 7.666 16.334 2.4 9.76 2.021zM10 16a2 2 0 1 1 .001-4.001A2 2 0 0 1 10 16zm6-2a2 2 0 1 1 .001-4.001A2 2 0 0 1 16 14z"></path></svg>`,
@@ -16,7 +18,8 @@ const bundles = [
         price:"$5,350 MXN"
     },
     {
-        title: "paquete 3",
+        id:"paquete3",
+        title: "Paquete 3",
         description: "Mini pizzas para todos",
         itemsIncluded: ["21 Mini pizzas","10L de bebida","20 Minis de 10 fichas","35 Fichas para cumpleanero","Regalo sorpresa","Anfitriona","4Hrs de evento","Desechables para comida","Mobiliario","Servicio de limpieza","Mantel para mesa principal","20 Postres de nieve"],
         icon: `<svg xmlns= width="24" height="24" fill="currentColor"><path d="M9.76 2.021a.995.995 0 0 0-.989.703L3.579 19.166a1 1 0 0 0 1.255 1.255l16.442-5.192a.991.991 0 0 0 .702-.988C21.6 7.666 16.334 2.4 9.76 2.021zM10 16a2 2 0 1 1 .001-4.001A2 2 0 0 1 10 16zm6-2a2 2 0 1 1 .001-4.001A2 2 0 0 1 16 14z"></path></svg>`,
@@ -24,7 +27,8 @@ const bundles = [
         price:"$4,600 MXN"
     },
     {
-        title: "paquete 4",
+        id:"paquete4",
+        title: "Paquete 4",
         description: "Pizza ",
         itemsIncluded: ["3 Pizzas XL","10L de bebida","20 Minis de 10 fichas","35 Fichas para cumpleanero","Regalo sorpresa","Anfitriona","4Hrs de evento","Desechables para comida","Mobiliario","Servicio de limpieza","Mantel para mesa principal","20 Postres de nieve","8 Centros de mesa","Torre de globos"],
         icon: `<svg xmlns= width="24" height="24" fill="currentColor"><path d="M9.76 2.021a.995.995 0 0 0-.989.703L3.579 19.166a1 1 0 0 0 1.255 1.255l16.442-5.192a.991.991 0 0 0 .702-.988C21.6 7.666 16.334 2.4 9.76 2.021zM10 16a2 2 0 1 1 .001-4.001A2 2 0 0 1 10 16zm6-2a2 2 0 1 1 .001-4.001A2 2 0 0 1 16 14z"></path></svg>`,
@@ -32,7 +36,8 @@ const bundles = [
         price:"$6,950 MXN"
     },
     {
-        title: "paquete 5",
+        id:"paquete5",        
+        title: "Paquete 5",
         description: "Paquete de Hotdogs+",
         itemsIncluded: ["84 Hotdogs","20L de bebida","20 Minis de 10 fichas","35 Fichas para cumpleanero","Regalo sorpresa","Anfitriona","4Hrs de evento","Desechables para comida","Mobiliario","Servicio de limpieza","Mantel para mesa principal","20 Postres de nieve","10 Centros de mesa","Torre de globos"],
         icon: `<svg xmlns= width="24" height="24" fill="currentColor"><path d="M9.76 2.021a.995.995 0 0 0-.989.703L3.579 19.166a1 1 0 0 0 1.255 1.255l16.442-5.192a.991.991 0 0 0 .702-.988C21.6 7.666 16.334 2.4 9.76 2.021zM10 16a2 2 0 1 1 .001-4.001A2 2 0 0 1 10 16zm6-2a2 2 0 1 1 .001-4.001A2 2 0 0 1 16 14z"></path></svg>`,
@@ -40,7 +45,8 @@ const bundles = [
         price:"$9,850 MXN"
     },
     {
-        title: "paquete 6",
+        id:"paquete6",
+        title: "Paquete 6",
         description: "Paquete de Pizzas Extra",
         itemsIncluded: ["4 Pizzas XL","Bandeja de spaguetti","20L de bebida","20 Minis de 10 fichas","35 Fichas para cumpleanero","Regalo sorpresa","Anfitriona","5Hrs de evento","Desechables para comida","Mobiliario","Servicio de limpieza","Mantel para mesa principal","20 Postres de nieve","10 Centros de mesa"],
         icon: `<svg xmlns= width="24" height="24" fill="currentColor"><path d="M9.76 2.021a.995.995 0 0 0-.989.703L3.579 19.166a1 1 0 0 0 1.255 1.255l16.442-5.192a.991.991 0 0 0 .702-.988C21.6 7.666 16.334 2.4 9.76 2.021zM10 16a2 2 0 1 1 .001-4.001A2 2 0 0 1 10 16zm6-2a2 2 0 1 1 .001-4.001A2 2 0 0 1 16 14z"></path></svg>`,
