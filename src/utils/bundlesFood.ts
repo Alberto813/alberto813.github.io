b15d87c6bb6534d3bc9a15b852a5c9241e493762
Alberto813 <albertomontiel813@gmail.com>
2024-01-30 16:35:33 -0800

    Add new data for Cafeteria section

---
+++ src/utils/bundlesFood.ts
@@ -0,0 +1,73 @@
+const bundlesFood = [
+    {
+        id: "com01",
+        name: "Combo Hot Dog",
+        items: [{
+                name: "Hot dog",
+                qty: "1",
+            },{
+                name: "Soda",
+                qty: "1",
+            },{
+                name: "Fichas",
+                qty: "3",
+            }],
+        price: "$55 MXN",
+    },{
+        id: "com02",
+        name: "Combo Nachos",
+        items: [{
+                name: "Nachos",
+                qty: "1",
+            },{
+                name: "Soda",
+                qty: "1",
+            },{
+                name: "Fichas",
+                qty: "3",
+            }],
+        price: "$55 MXN",
+    },{
+        id: "com03",
+        name: "Combo Palomitas",
+        items: [{
+                name: "Palomitas Grandes",
+                qty: "1",
+            },{
+                name: "Soda",
+                qty: "1",
+            },{
+                name: "Fichas",
+                qty: "3",
+            }],
+        price: "$55 MXN",
+    },{
+        id: "com04",
+        name: "Combo hamburguesa",
+        items: [{
+                name: "hamburguesa de pechuga",
+                qty: "1",
+            },{
+                name: "Soda",
+                qty: "1",
+            },{
+                name: "Papas fritas",
+                qty: "1",
+            }],
+        price: "$155 MXN",
+    },{
+        id: "com05",
+        name: "Combo hamburguesa 2",
+        items: [{
+                name: "Hamburguesa Carne",
+                qty: "1",
+            },{
+                name: "Soda",
+                qty: "1",
+            },{
+                name: "Papas fritas",
+                qty: "1",
+            }],
+        price: "$185 MXN",
+    },
+]
---
+++ src/utils/drinks.ts
@@ -0,0 +1,62 @@
+const drinks = [
+    {
+        id: "d01",
+        name: "Powerade",
+        price: "$28 MXN",
+    },
+    {
+        id: "d02",
+        name: "Coca Cola vidrio",
+        price: "$15 MXN",
+    },
+    {
+        id: "d03",
+        name: "Fuzetea",
+        price: "$25 MXN",
+    },
+    {
+        id: "d04",
+        name: "Soda lata",
+        price: "$20 MXN",
+    },
+    {
+        id: "d05",
+        name: "Soda 400ml",
+        price: "$ MXN",
+    },
+    {
+        id: "d06",
+        name: "Agua chica",
+        price: "$18 MXN",
+    },
+    {
+        id: "d07",
+        name: "Agua 1L",
+        price: "$25 MXN",
+    },
+    {
+        id: "d08",
+        name: "Pulpy",
+        price: "$22 MXN",
+    },
+    {
+        id: "d09",
+        name: "Jugo del valle",
+        price: "$20 MXN",
+    },
+    {
+        id: "d10",
+        name: "Caprisun",
+        price: "$20 MXN",
+    },
+    {
+        id: "d11",
+        name: "Santa clara",
+        price: "$20 MXN",
+    },
+    {
+        id: "d12",
+        name: "Paupau",
+        price: "$10 MXN / $15 MXN",
+    }
+]
