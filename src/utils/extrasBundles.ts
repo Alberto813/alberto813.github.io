7bc258d990d0def7039945108740081654ee6f9b
Alberto813 <albertomontiel813@gmail.com>
2024-01-30 16:34:38 -0800

    Add new sections and info for bundlespage, not styled yet.

---
+++ src/utils/extrasBundles.ts
@@ -0,0 +1,57 @@
+const extrasBundles = [
+    {
+        id: "ex01",
+        name: "Bandeja de spaguetti",
+        price: "$450 MXN",
+    },{
+        id: "ex02",
+        name: "Hot dog",
+        price: "$28 MXN",
+    },{
+        id: "ex03",
+        name: "Pizza XL",
+        price: "$250 MXN",
+    },{
+        id: "ex04",
+        name: "Mini pizza",
+        price: "$60 MXN",
+    },{
+        id: "ex05",
+        name: "Nieve",
+        price: "$12 MXN",
+    },{
+        id: "ex06",
+        name: "Agua 10L",
+        price: "$200 MXN",
+    },{
+        id: "ex07",
+        name: "Torre de globos",
+        price: "$600 MXN",
+    },{
+        id: "ex08",
+        name: "Arco de globos",
+        price: "$800 MXN",
+    },{
+        id: "ex09",
+        name: "Centro de mesa",
+        price: "$70 MXN",
+    },{
+        id: "ex10",
+        name: "60 Fichas",
+        price: "$320 MXN",
+    },{
+        id: "ex11",
+        name: "Descorche 20 a 30 personas",
+        price: "$800 MXN",
+    },{
+        id: "ex12",
+        name: "Descorche 30 a 50 personas",
+        price: "$1,200 MXN",
+    },{
+        id: "ex13",
+        name: "Descorche mas de 50 personas",
+        price: "$2,000 MXN",
+    }
+]
+
+export { extrasBundles };
---
+++ src/utils/pktsnack.ts
@@ -0,0 +1,13 @@
+const pktsnack = [
+    {
+        id: "pkt01",
+        name: "10 Ordenes de nachos",
+        price: "$450 MXN",
+    },{
+        id: "pkt02",
+        name: "5 Ordenes de nachos y papas",
+        price: "$500 MXN",
+    }
+]
+
+export { pktsnack };
